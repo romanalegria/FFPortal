2867c92e040249d6b6d2679a6f2f9b846896ddaa
Emilio Roman Alegria <eranet1973@hotmail.com>
2022-03-08 11:10:32 -0300

    add vehicletype

---
+++ FFPortal.API/Data/Entities/VehicleType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FFPortal.API.Data.Entities
+{
+    public class VehicleType
+    {
+        public int Id { get; set; }
+
+        
+        [Display(Name ="Tipo de véhiculo")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener mas de {1} carácteres")]
+        [Required(ErrorMessage ="El campo {0} es obligatorio.")]
+        public string Description { get; set; }
+    }
+}
